cb3ca93d23144e24e676e681658bb0f6434ced0a
dapranol <dapranol@oregonstate.edu>
2022-04-25 12:40:41 -0700

    updat lab2

---
+++ PythonAndNumpy2/calculator.py
@@ -0,0 +1,10 @@
+#problem 3
+
+def sum_numbers(a, b):
+    return(a + b) 
+def product_numbers(c, d):
+    return(c * d)
+
+
+
+
---
+++ StandardLibrary/calculator.py
@@ -0,0 +1,6 @@
+#problem 3                                                                      
+                                                                                
+def sum_numbers(a, b):                                                          
+    return(a + b)                                                               
+def product_numbers(c, d):                                                      
+    return(c * d) 
