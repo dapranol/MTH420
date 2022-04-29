2751d2c686eb467df0c5e2150df3f55de47e5cf2
dapranol <dapranol@oregonstate.edu>
2022-04-29 10:42:08 -0700

    lab 3

--- PythonAndNumpy2/python_numpy2.py
+++ PythonAndNumpy2/python_numpy2.py
@@ -121,4 +121,36 @@ if __name__ == "__main__":
     Matrix_3 = np.vstack((I, Z_3, C))
     print(prob6())
 #problem 8
-    
+
+    def largest_adj(m):
+        def mul_max(largest, *entries):
+            prod = np.prod(entries)
+            
+            if prod > largest:
+                largest = prod
+
+            return largest 
+
+        largest = 0
+        for r in range(len(m)):
+            for c in range(len(m[0])):
+                if r < len(m) - 3 and c < len(m[0]) - 3:
+                    # Get diagonal top left
+                    largest = mul_max(largest, *[m[r+i][c+i] for i in range(4)])
+
+                    # Get diagonal top right
+                    largest = mul_max(largest, m[r+3][c], m[r+1][c+2], m[r+2][c+1], m[r][c+3])
+
+                if r < len(m) - 3:
+                    # Get vertical products
+                    largest = mul_max(largest, *[m[r+i][c] for i in range(4)])
+
+                if c < len(m) - 3:
+                    # Get horizontal products
+                    largest = mul_max(largest, *[m[r][c+i] for i in range(4)])
+
+        return largest
+            
+    # import the matrix called m
+    m = np.load("grid.npy")
+    print(largest_adj(m))
